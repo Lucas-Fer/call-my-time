908d501d69893ee08956bca4b69b9f25adb621f0
lucas-fer <alikacildes@gmail.com>
2023-06-27 18:44:39 -0300

    feat and config: save user cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/Prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -24,6 +25,11 @@ export default async function handler(
       username,
       userfullname,
     },
+  })
+
+  setCookie({ res }, '@call-my-time', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)
