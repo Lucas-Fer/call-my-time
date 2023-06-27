1e75a70da746fdc397a286d070548ed4f4294086
lucas-fer <alikacildes@gmail.com>
2023-06-27 15:50:06 -0300

    fix: 'userfullname'

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -64,7 +64,7 @@ export default function Register() {
           <TextInput placeholder="Seu nome" {...register('userfullname')} />
 
           {errors.userfullname && (
-            <FormError size="sm">{errors.username.message}</FormError>
+            <FormError size="sm">{errors.userfullname.message}</FormError>
           )}
         </label>
 
