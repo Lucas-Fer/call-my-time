430172536d6ae55f3ec1f808564a08663e6110da
lucas-fer <alikacildes@gmail.com>
2023-06-27 16:48:36 -0300

    setup: axios and config

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Teste' })
+  return res.status(200).json(req.body)
 }
